d1279fb523712f043dd3668007590aac32959ea3
liuyuekun <yuekunliu9@gmail.com>
2025-10-21 19:13:09 +0800

    my-first-code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+function App() {
+  return (
+    <div className="App">
+      this is a app
+      <Button type="primary">this is button</Button>
+    </div>
+  );
+}
+
+export default App;
